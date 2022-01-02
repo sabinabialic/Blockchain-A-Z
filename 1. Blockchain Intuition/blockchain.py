08ce6051d74958d898efaff962a29bc192847716
Sabina <sabina.bialic@gmail.com>
2022-01-02 11:36:36 -0500

    First Commit
    Starting module 1

---
+++ 1. Blockchain Intuition/blockchain.py
@@ -0,0 +1,12 @@
+# Module 1 - Create a Blockchain
+
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+# Part 2 - Mining our Blockchain
